97867597658e2a3072e487e1218783f50a1d29ea
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-05-06 21:52:12 +0530

    init

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,41 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Binary Search
-//Works On Sorted Array
+// Binary Search
+// Works On Sorted Array
 
-int BinarySearch(int A[] ,int firsti,int lasti, int key){
-int mid=0;
+int BinarySearch(int A[], int firsti, int lasti, int key)
+{
+    int mid = 0;
 
-if(firsti<=lasti){
-    mid = (firsti+lasti)/2;
+    if (firsti <= lasti)
+    {
+        mid = (firsti + lasti) / 2;
+    }
+
+    if (A[mid] == key)
+    {
+        return mid;
+    }
+
+    if (A[mid] < key)
+    {
+        return BinarySearch(A, mid + 1, lasti, key);
+    }
+
+    if (A[mid] < key)
+    {
+        return BinarySearch(A, firsti, mid - 1, key);
+    }
+
+    return -1;
 }
 
-if(A[mid]==key){
-    return mid;
-}
-
-if(A[mid]<key){
- return BinarySearch(A ,mid+1,lasti, key);
-}
-
-if(A[mid]<key){
- return BinarySearch(A ,firsti,mid-1, key);
-}
-
-return -1;
-}
-
-int main(){
-    int A[] = {5,7,19,21,29,34};
-    int firsti=0, lasti=5;
+int main()
+{
+    int A[] = {5, 7, 19, 21, 29, 34};
+    int firsti = 0, lasti = 5;
     int key = 27;
-int result = BinarySearch(A ,firsti,lasti, key);
+    int result = BinarySearch(A, firsti, lasti, key);
 
-if(result==-1){
-cout<<"Not Found";
-} else{
-cout<<key<<" Found At Index "<<BinarySearch(A ,firsti,lasti, key);
-}
-
+    if (result == -1)
+    {
+        cout << "Not Found";
+    }
+    else
+    {
+        cout << key << " Found At Index " << BinarySearch(A, firsti, lasti, key);
+    }
 }
