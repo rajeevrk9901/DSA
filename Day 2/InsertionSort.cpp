dc6f4c5ed8107a6d3a5f75752be06abc2b5609b4
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-05-07 15:02:49 +0530

    Formate Code

--- Day 2/InsertionSort.cpp
+++ Day 2/InsertionSort.cpp
@@ -1,6 +1,5 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 void display(int A[], int n)
 {
@@ -10,29 +9,28 @@ void display(int A[], int n)
     }
 }
 
-void InsertionSort(int A[], int n){
+void InsertionSort(int A[], int n)
+{
 
-for(int i=1; i<n; i++){
-
-int j=i-1;
-
-int x= A[i];
-
-while(j>-1 && A[j]>x){
-    A[j+1]=A[j];
-    j--;
+    for (int i = 1; i < n; i++)
+    {
+        int j = i - 1;
+        int x = A[i];
+        while (j > -1 && A[j] > x)
+        {
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[j + 1] = x;
+    }
 }
-A[j+1]=x;
-}
-}
-
 
 int main()
 {
-    int A[] = {5, 1, 9, 77, 4, 11, 88, 34};
-    //int n = 8;
+    int A[] = {5, 1, 9, 77, 4, 11, 88, 21, 34};
+    // int n = 9;
     int n = sizeof(A) / sizeof(A[0]);
-    cout<<n<<"\n";
+    cout << n << "\n";
     InsertionSort(A, n);
     display(A, n);
 }
