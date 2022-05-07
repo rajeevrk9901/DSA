a4851723fc716360eea1cd00d3e7711be33a37f6
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-05-07 14:56:34 +0530

    init

---
+++ Day 2/InsertionSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+
+void display(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+}
+
+void InsertionSort(int A[], int n){
+
+for(int i=1; i<n; i++){
+
+int j=i-1;
+
+int x= A[i];
+
+while(j>-1 && A[j]>x){
+    A[j+1]=A[j];
+    j--;
+}
+A[j+1]=x;
+}
+}
+
+
+int main()
+{
+    int A[] = {5, 1, 9, 77, 4, 11, 88, 34};
+    //int n = 8;
+    int n = sizeof(A) / sizeof(A[0]);
+    cout<<n<<"\n";
+    InsertionSort(A, n);
+    display(A, n);
+}
