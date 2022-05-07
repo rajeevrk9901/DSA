f4ec6de4caa426224c033f072c66369aa6abb5c8
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-05-07 15:04:19 +0530

    Format Code

--- Day 2/InsertionSort.cpp
+++ Day 2/InsertionSort.cpp
@@ -30,7 +30,7 @@ int main()
     int A[] = {5, 1, 9, 77, 4, 11, 88, 21, 34};
     // int n = 9;
     int n = sizeof(A) / sizeof(A[0]);
-    cout << n << "\n";
+    cout << "Total No.: "<< n << "\n";
     InsertionSort(A, n);
     display(A, n);
 }
