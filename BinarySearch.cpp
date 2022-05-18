0941d8f9b5a69b0bb5a6ca20ab7dca6186cff649
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-05-18 19:08:43 +0530

    init

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
 
-// Binary Search
-// Works On Sorted Array
-
 int BinarySearch(int A[], int firsti, int lasti, int key)
 {
+
     int mid = 0;
+
     if (firsti <= lasti)
     {
+
         mid = (firsti + lasti) / 2;
     }
+
+    // cout << mid;
 
     if (A[mid] == key)
     {
         return mid;
     }
 
-    if (A[mid] < key)
+    if (A[mid] > key)
     {
         return BinarySearch(A, mid + 1, lasti, key);
     }
 
-    if (A[mid] > key)
+    if (A[mid] < key)
     {
         return BinarySearch(A, firsti, mid - 1, key);
     }
@@ -30,13 +32,13 @@ int BinarySearch(int A[], int firsti, int lasti, int key)
     return -1;
 }
 
-// Github Test
-
 int main()
 {
-    int A[] = {5, 7, 19, 21, 29, 34};
-    int firsti = 0, lasti = 5;
-    int key = 21;
+
+    int A[] = {2, 5, 7, 9, 11, 15, 17};
+    int firsti = 0;
+    int lasti = 6;
+    int key = 5;
     int result = BinarySearch(A, firsti, lasti, key);
 
     if (result == -1)
@@ -47,4 +49,6 @@ int main()
     {
         cout << key << " Found At Index " << BinarySearch(A, firsti, lasti, key);
     }
+
+    return 0;
 }
